8ed7521e59c2ac2cc0ae22039c976bc4dc869997
Vitaliy Gorbenko <devgorbenko@gmail.com>
2018-12-16 21:50:22 +0300

    Added stripped down mvc react project template

---
+++ LastApple/ClientApp/src/components/Layout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
+
+export class Layout extends Component {
+  displayName = Layout.name
+
+  render() {
+    return (
+      <Grid fluid>
+        <Row>
+          <Col sm={3}>
+          </Col>
+          <Col sm={9}>
+            {this.props.children}
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
