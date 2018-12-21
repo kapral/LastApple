848e5b68f8fcdb5f107340184ea412ef43549eef
Vitaliy Gorbenko <devgorbenko@gmail.com>
2018-12-22 00:42:45 +0300

    Layout styles and MusicKit integration

--- LastApple/ClientApp/src/components/Home.js
+++ LastApple/ClientApp/src/components/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import {PlayerControl} from "./Player/PlayerControl";
+
 
 export class Home extends Component {
   displayName = Home.name
 
   render() {
-    return (
-      <div></div>
-    );
+      return <div style={{ marginTop: '30px' }}>
+          <h4 style={{ color: '#666' }}>Last Apple</h4>
+            <PlayerControl/>
+      </div>;
   }
 }
