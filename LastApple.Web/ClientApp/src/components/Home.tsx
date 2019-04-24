d5bd84433303165fa5da34b401a5e35ff36ae392
Vitaliy Gorbenko <devgorbenko@gmail.com>
2019-04-24 21:54:11 +0300

    Added similar artists station

--- LastApple.Web/ClientApp/src/components/Home.tsx
+++ LastApple.Web/ClientApp/src/components/Home.tsx
@@ -4,6 +4,7 @@ import { StationsList } from "./StationsList";
 import { SingleArtist } from "./Stations/SingleArtist";
 import { Col, Grid, Row } from "react-bootstrap";
 import { Tag } from "./Stations/Tag";
+import { SimilarArtists } from "./Stations/SimilarArtists";
 
 export class Home extends Component {
     displayName = Home.name
@@ -16,6 +17,9 @@ export class Home extends Component {
                     <Row>
                         <Col md={6}><SingleArtist/></Col>
                         <Col md={6}><Tag/></Col>
+                    </Row>
+                    <Row>
+                        <Col md={6}><SimilarArtists/></Col>
                     </Row>
                 </Grid>
             </StationsList>
