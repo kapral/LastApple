d2d7cfc713adb63601be1cdf38cd8e5e9af3f1ab
Vitaliy Gorbenko <devgorbenko@gmail.com>
2019-09-22 18:25:08 +0300

    implemented external auth for mobile

--- LastApple.Web/ClientApp/src/index.tsx
+++ LastApple.Web/ClientApp/src/index.tsx
@@ -6,6 +6,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+(window as any).handleOpenURL = function(href) {
+  const url = new URL(href);
+  const sessionId = url.searchParams.get('sessionId');
+
+  if (sessionId) {
+      localStorage.setItem('SessionId', sessionId);
+
+      window.location.reload();
+    }
+};
+
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
