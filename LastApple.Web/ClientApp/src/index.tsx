1c02c93aad48cc7aca8fc3394f61c7d836c1b33d
Vitaliy Gorbenko <devgorbenko@gmail.com>
2019-09-22 18:23:41 +0300

    Removed BrowserRouter

--- LastApple.Web/ClientApp/src/index.tsx
+++ LastApple.Web/ClientApp/src/index.tsx
@@ -3,17 +3,13 @@ import '@fortawesome/fontawesome-free/js/all.js'
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import environment from './components/Environment';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={environment.baseUrl}>
-    <App />
-  </BrowserRouter>,
+    <App />,
   rootElement);
 
 registerServiceWorker();
