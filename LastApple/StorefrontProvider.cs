1b52f2ce0fd11d48d9e8c59e70b07ad5b66ee34d
Vitaliy Gorbenko <devgorbenko@gmail.com>
2019-11-23 23:08:32 +0300

    #45: Implemented storefront provider

---
+++ LastApple/StorefrontProvider.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+
+namespace LastApple
+{
+    public class StorefrontProvider : IStorefrontProvider
+    {
+        private const string DefaultStorefront = "us";
+            
+        private readonly ISessionProvider sessionProvider;
+
+        public StorefrontProvider(ISessionProvider sessionProvider)
+        {
+            this.sessionProvider = sessionProvider ?? throw new ArgumentNullException(nameof(sessionProvider));
+        }
+
+        public async Task<string> GetStorefront()
+        {
+            var session = await sessionProvider.GetSession();
+            return session?.MusicStorefrontId ?? DefaultStorefront;
+        }
+    }
+}
