38f815fdbdab3b96c9c8d8251779f5489a5ab7f5
Kirill Gorbenko <kgorbenkodev@proton.me>
2023-07-20 08:01:28 +0300

    Get rid of cordova

--- LastApple.Web/ClientApp/src/Mobile/MobileUtil.ts
+++
@@ -1,5 +0,0 @@
-import environment from '../Environment';
-
-export class MobileUtil {
-    static formatAppUrl = () => `${environment.mobileAppSchema}?sessionId=${localStorage.getItem('SessionId')}`;
-}
--- LastApple.Web/ClientApp/src/index.tsx
+++ LastApple.Web/ClientApp/src/index.tsx
@@ -5,39 +5,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import env from './Environment';
-import { playbackEventMediator } from './PlaybackEventMediator';
 import { assertNonNullable } from './utils/mics';
 import { AppContextProvider } from './AppContext';
 
-window.handleOpenURL = href => {
-    const url = new URL(href);
-    const sessionId = url.searchParams.get('sessionId');
-
-    if (sessionId) {
-        setTimeout(() => {
-            window.SafariViewController.hide();
-            localStorage.setItem('SessionId', sessionId);
-
-            window.location.reload();
-        }, 0);
-    }
-};
-
-const cordova = window.cordova;
-document.addEventListener('deviceready', function () {
-    playbackEventMediator.subscribePlayStart(() => cordova.plugins.backgroundMode.enable());
-    playbackEventMediator.subscribePlayEnd(() => cordova.plugins.backgroundMode.disable());
-
-    cordova.plugins.backgroundMode.onactivate = () => {};
-    cordova.plugins.backgroundMode.ondeactivate = () => {};
-}, false);
-
 const rootElement = document.getElementById('root');
 assertNonNullable(rootElement);
-
-if (env.isMobile)
-    rootElement.classList.add('mobile');
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
