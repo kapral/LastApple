4c0e022e28dbc53ae65e60c78d8f6311e84028f7
Kirill Gorbenko <kgorbenkodev@proton.me>
2023-08-09 09:23:46 +0300

    Remove privacy page

--- LastApple.Web/ClientApp/src/App.tsx
+++ LastApple.Web/ClientApp/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {BrowserRouter, matchPath, Route, RouteComponentProps, Switch} from 'react-router-dom';
+import {BrowserRouter, matchPath, Route, RouteComponentProps} from 'react-router-dom';
 import {Layout} from './components/Layout';
 import {Home} from './components/Home';
 import {Settings} from "./components/Settings";
-import {PrivacyPolicy} from './components/PrivacyPolicy';
 import {useStartupAppleAuthenticationCheck} from "./hooks/useStartupAppleAuthenticationCheck";
 import {useStartupLastfmAuthenticationCheck} from "./hooks/useStartupLastfmAuthenticationCheck";
-import {homeRoute, privacyRoute, settingsRoute, stationRoute} from "./routes";
+import {homeRoute, settingsRoute, stationRoute} from "./routes";
 import {NowPlaying} from "./components/NowPlaying";
 
 export const App: React.FunctionComponent = () => {
@@ -18,7 +17,6 @@ export const App: React.FunctionComponent = () => {
             <Layout>
                 <Route path={homeRoute} exact component={Home}/>
                 <Route path={settingsRoute} exact component={Settings} />
-                <Route path={privacyRoute} exact component={PrivacyPolicy} />
 
                 <Route render={(props: RouteComponentProps) => {
                         const match = matchPath(props.location.pathname, {
--- LastApple.Web/ClientApp/src/routes.ts
+++ LastApple.Web/ClientApp/src/routes.ts
@@ -3,7 +3,3 @@ export const homeRoute = '/';
 export const stationRoute = '/station/:stationId';
 
 export const settingsRoute = '/settings';
-
-export const privacyRoute = '/privacy';
-
-export const noNavRoutes = [privacyRoute];
